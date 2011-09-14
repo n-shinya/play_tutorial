bccc9e0bd28591f88c6d88341c1efad56722ca1a
nishinaka.shinya@gmail.com <nishinaka.shinya@gmail.com>
2011-09-14 15:43:03 +0900

    second.

---
+++ app/models/Post.java
@@ -0,0 +1,43 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Post extends Model {
+	
+	public String title;
+	
+	public Date postedAt;
+	@Lob
+	public String content;
+	@ManyToOne
+	public User author;
+	
+	@OneToMany(mappedBy="post", cascade=CascadeType.ALL)
+	public List<Comment> comments;
+	
+	public Post(User author, String title, String content) {
+		this.comments = new ArrayList<Comment>();
+		this.title = title;
+		this.postedAt = new Date();
+		this.content = content;
+		this.author = author;
+	}
+	
+	public Post addComment(String author, String content) {
+		Comment comment = new Comment(this, author, content).save();
+		this.comments.add(comment);
+		this.save();
+		return this;
+	}
+}
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,13 +1,82 @@
-import org.junit.*;
-import java.util.*;
-import play.test.*;
-import models.*;
+import java.util.List;
+
+import models.Comment;
+import models.Post;
+import models.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.test.Fixtures;
+import play.test.UnitTest;
 
 public class BasicTest extends UnitTest {
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
+	@Before
+	public void setup() {
+		Fixtures.deleteDatabase();
+	}
+	
+	@Test
+	public void createAndRetrieveUser() {
+		new User("nishinaka.shinya@gmail.com", "1234", "Nishinaka Shinya").save();		
+		User user = User.find("byEmail", "nishinaka.shinya@gmail.com").first();
+		assertNotNull(user);
+		assertEquals("Nishinaka Shinya", user.fullname);
+	}
 
+	@Test
+	public void tryConnectAsUser() {
+		new User("nishinaka.shinya@gmail.com", "1234", "Bob").save();
+		
+		assertNotNull(User.connect("nishinaka.shinya@gmail.com", "1234"));
+		assertNull(User.connect("hoge@gmail.com", "1234"));
+		assertNull(User.connect("nishinaka.shinya@gmail.com", "5678"));
+	}
+	
+	@Test
+	public void createPost() {
+		User user = new User("nishinaka.shinya@gmail.com", "1234", "Nishinaka Shinya").save();
+		new Post(user, "MyFirstPost", "Hello!").save();
+		assertEquals(1, Post.count());
+		
+		List<Post> posts = Post.find("byAuthor", user).fetch();
+		
+		assertEquals(1, posts.size());
+		Post firstPost = posts.get(0);
+		assertNotNull(firstPost);
+		assertEquals(user, firstPost.author);
+		assertEquals("MyFirstPost", firstPost.title);
+		assertEquals("Hello!", firstPost.content);
+		assertNotNull(firstPost.postedAt);
+	}
+	
+	@Test
+	public void useTheCommentsRelaton() {
+		User user = new User("nishinaka.shinya@gmail.com","1234","Nishinaka Shinya").save();
+		Post post = new Post(user, "MyFirstPost", "Hello!").save();
+		post.addComment("Jeff", "Nice Post!!");
+		post.addComment("Tom", "I knew that!");
+		
+		assertEquals(1, User.count());
+		assertEquals(1, Post.count());
+		assertEquals(2, Comment.count());
+		
+		post = Post.find("byAuthor", user).first();
+		assertNotNull(post);
+		
+		post.delete();
+		assertEquals(1, User.count());
+		assertEquals(0, Post.count());
+		assertEquals(0, Comment.count());
+	}
+	
+	@Test
+	public void yamlTest() {
+		Fixtures.loadModels("data.yml");
+		
+		assertEquals(2, User.count());
+		assertEquals(3, Post.count());
+		assertEquals(3, Comment.count());
+	}
 }
